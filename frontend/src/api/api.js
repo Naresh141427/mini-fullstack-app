eb66a8edc6d3bc9949e938e2bebc0ee005cc4c21
Naresh141427 <a.nareshindian@example.com>
2025-12-06 16:56:52 +0530

    added live backend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://mini-fullstack-app.onrender.com",
+    baseURL: "https://mini-fullstack-app.onrender.com/api",
 });
 
 export default api;
